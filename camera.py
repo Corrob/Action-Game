7745325660f78bf8bef85e68ce358da0ff437296
Cory Harasha <cory.harasha@gmail.com>
2014-03-10 16:07:55 -0700

    Change background to be gray instead of black

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
 from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle
+from kivy.graphics import Rectangle, Color
 
 class Camera(Widget):
     tiles = []
@@ -12,6 +12,12 @@ class Camera(Widget):
         self.tile_height = 8
         self.graphics = graphics
         self.action_map = action_map
+        self.canvas.add(Color(0.13, 0.12, 0.15))
+        self.background = Rectangle()
+        self.background.pos = (0, 0)
+        self.background.size = (100, 100)
+        self.canvas.add(self.background)
+        self.canvas.add(Color(1, 1, 1))
 
         for x in range(0, self.CAMERA_WIDTH_IN_TILES):
             self.tiles.append([])
@@ -33,6 +39,7 @@ class Camera(Widget):
                 (width=self.tile_width,height=self.tile_height))
         self.pos = (0, 0)
         self.size = (root.width, root.height)
+        self.background.size = self.size
         self.update_tiles()
 
     def update_tiles(self):
